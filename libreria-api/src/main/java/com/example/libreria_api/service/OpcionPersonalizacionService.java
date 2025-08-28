1cb35138d8fbe1fc23538786a4dc4fe343e86204
Fabian Sanchez <dfabiansg@gmail.com>
2025-08-28 10:15:50 -0500

    Actualice personalizacion controller service. Actualice gitignore para ignorar application.properties

--- libreria-api/src/main/java/com/example/libreria_api/service/OpcionPersonalizacionService.java
+++ libreria-api/src/main/java/com/example/libreria_api/service/OpcionPersonalizacionService.java
@@ -26,4 +26,8 @@ public class OpcionPersonalizacionService {
             return opcionPersonalizacionRepository.save(opcionExistente);
         }).orElse(null);
     }
+
+    public void eliminar(Integer id) {
+        opcionPersonalizacionRepository.deleteById(id);
+    }
 }
