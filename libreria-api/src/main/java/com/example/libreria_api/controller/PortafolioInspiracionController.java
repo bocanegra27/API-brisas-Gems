602b1918aa3ba083101f94acd8f6dfc154ef8f44
Natalia <natalia.qek@gmail.com>
2025-08-29 17:58:32 -0500

    CRUD completo tabla portafolio_inspiracion

---
+++ libreria-api/src/main/java/com/example/libreria_api/controller/PortafolioInspiracionController.java
@@ -0,0 +1,38 @@
+package com.example.libreria_api.controller;
+
+
+import com.example.libreria_api.model.PortafolioInspiracion;
+import com.example.libreria_api.model.Rol;
+import com.example.libreria_api.service.PortafolioInspiracionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PortafolioInspiracionController {
+
+    @Autowired
+    private PortafolioInspiracionService portafolioInspiracionService;
+
+    @GetMapping("/portafolio")
+    public List<PortafolioInspiracion> obtenerPortafolio() {
+        return portafolioInspiracionService.obtenerPortafolio();
+    }
+
+    @PostMapping("/portafolio")
+    public PortafolioInspiracion guardarPortafolio(@RequestBody PortafolioInspiracion portafolioInspiracion) {
+        return portafolioInspiracionService.guardarPortafolio(portafolioInspiracion);
+    }
+
+    @PutMapping("/portafolio/{id}")
+    public PortafolioInspiracion actualizarPortafolio(@PathVariable Integer id, @RequestBody PortafolioInspiracion portafolioInspiracion) {
+        return portafolioInspiracionService.actualizar(id, portafolioInspiracion);
+    }
+
+    @DeleteMapping("/portafolio/{id}")
+    public void eliminarPortafolio(@PathVariable Integer id){
+        portafolioInspiracionService.eliminarPortafolio(id);
+    }
+}
+
