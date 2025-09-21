7d94b645f89a72f18119e78757f682a461af931b
bocanegra <bocanegrajohan123@gmail.com>
2025-09-20 19:42:03 -0500

    implementacion de autenticacion API

--- libreria-api/src/main/java/com/example/libreria_api/config/SecurityConfig.java
+++ libreria-api/src/main/java/com/example/libreria_api/config/SecurityConfig.java
@@ -1,32 +1,40 @@
 package com.example.libreria_api.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod; // <-- IMPORTANTE AÑADIR ESTE IMPORT
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        // Endpoints públicos
+                        .requestMatchers("/api/auth/**").permitAll()
+                        /*.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll() // agregar usuarios nuevos*/
 
-        http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(auth ->
-                auth.anyRequest()
-                        .permitAll()
-        );
+                         .anyRequest().authenticated()
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
-
